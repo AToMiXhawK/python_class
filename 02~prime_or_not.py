eb56ed415ad6b85813b744a93aaec3b72c8bd718
AToMiXhawK <githinmanesh@gmail.com>
2019-05-10 08:54:37 +0530

    Update code from py2 to py3

--- 02~prime_or_not.py
+++ 02~prime_or_not.py
@@ -1,15 +1,15 @@
 def prime(n):
 	if n==1 or n==0:
-		print n,"is Neither Prime Nor Composite"
+		print(n,"is Neither Prime Nor Composite")
 		return
 	i=2
 	while i<=n/2:
 		if n%i==0:
-			print n,"is Not a Prime Number"
+			print(n,"is Not a Prime Number")
 			return
 		else:
 			i+=1
-	print n,"is a Prime Number"
+	print(n,"is a Prime Number")
 	return
 	
 n=input("Enter a Number: ")
